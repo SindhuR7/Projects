8217ad62526c6c9b3821359ff48be2441510c195
SindhuR7 <alirazasindhu066@gmail.com>
2025-08-07 00:48:16 +0500

    Zummi

---
+++ Zummi/zummi.js
@@ -0,0 +1,39 @@
+const arrow = document.getElementById("click-arrow")
+const show = document.getElementById("show")
+const arrow2 = document.getElementById("click-arrow2")
+const show2 = document.getElementById("show2")
+const showBar = document.getElementById("show")
+const closeBar = document.getElementById("close-bar")
+const sideBar = document.getElementById("side-bar")
+let right = false
+function closeSidebar(){
+    sideBar.style.display = "none"
+}
+function showSidebar(){
+    sideBar.style.display = "block"
+}
+function rotateDown(){
+    arrow.style.rotate = "270deg"
+    arrow.style.color = "#fff"
+}
+function rotateBack(){
+    arrow.style.rotate = "180deg"
+    arrow.style.color = "#fff"
+}
+function showSubmenu(){
+    show.style.display = "flex"
+}
+function closeSubmenu(){
+    show.style.display = "none"
+}
+
+arrow.addEventListener("click",()=>{
+    right = !right
+
+    if(right) showSubmenu()
+        else closeSubmenu()
+    if(right) rotateDown()
+        else rotateBack()
+})
+closeBar.addEventListener("click",closeSidebar)
+showBar.addEventListener("click",showSidebar)
